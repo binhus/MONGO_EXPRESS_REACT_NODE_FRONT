1660036fd333d837f6bd1d677e73e93e596cbbc9
binhus <eng.robsonwilli@gmail.com>
2021-01-24 23:53:50 -0300

    deploy no firebase

--- mern-app/src/Components/Page.jsx
+++ mern-app/src/Components/Page.jsx
@@ -1,9 +1,41 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-const Page = () => {
-  const [name, setName] = useState(null);
+const Page = ({ match }) => {
+  const { name } = match.params;
+  const [displayedName, setDisplayedName] = useState(null);
+  const [message, setMessage] = useState(null);
 
-  return name ? <p>{`This page belongs to ${name}`}</p> : <p>loading...</p>;
+  console.log(name);
+
+  const pageHandle = async (param) => {
+    const page = await fetch(`https://mern-api-robson.herokuapp.com/${param}`, {
+      mode: 'cors',
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'http://localhost:3000/',
+      },
+    })
+      .then((result) => result.json())
+      .then((json) => Promise.resolve(json))
+      .catch((err) => Promise.reject(err));
+    setDisplayedName(page.name);
+    if (page.message) {
+      setMessage(page.message);
+    }
+  };
+
+  useEffect(() => {
+    if (!displayedName) {
+      pageHandle(name);
+    }
+  });
+
+  return displayedName ? (
+    <p>{`This page belongs to ${displayedName}`}</p>
+  ) : (
+    message ? <p>{message}</p> : <p>Loading...</p>
+  );
 };
 
 export default Page;
